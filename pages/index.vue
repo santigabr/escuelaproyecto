1dcb5a16ab9189ae0fcab1cb19a567cde4aaa46f
EmanuelRaffetto <emanuelraffetto18@gmail.com>
2024-10-29 15:20:57 -0300

    Ajustando los tamaños del texto principal

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <main class="bg-rhino-200">
     <div class="w-full min-h-screen animate-slidein opacity-0 [--slidein-delay:200ms] lg:grid lg:grid-cols-2 place-content-center lg:mx-auto gap-5 justify-center max-w-6xl px-3 xl:px-0">
       <div class="flex flex-col justify-center gap-5">
-        <h2 class="text-6xl pb-2 font-[900] text-rhino-950 max-w-[500px] tracking-tighter">
+        <h2 class="lg:text-6xl md:text-5xl text-4xl pb-2 font-[900] text-rhino-950 md:max-w-[500px] tracking-tighter">
           Todo lo que eres. En un simple enlace.
         </h2>
         <p class="max-w-[500px] text-rhino-900 text-lg tracking-tighter font-medium">
