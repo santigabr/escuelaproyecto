282fbde3dac2fa2df98da3d8f222809b1fd02806
Santino <89117328+santigabr@users.noreply.github.com>
2024-10-29 14:22:22 -0700

    Configurando tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,26 @@ export default {
   content: [],
   theme: {
     extend: {
-
+      colors: {
+        blackk: '#101114',
+        durazno: '#ff9264',
+        rhino: {
+          50: '#f1f7fd',
+          100: '#e0eef9',
+          200: '#c9e1f4',
+          300: '#a3cfed',
+          400: '#78b3e2',
+          500: '#5898d9',
+          600: '#437ecd',
+          700: '#3a6bbb',
+          800: '#345899',
+          900: '#2b4570',
+          950: '#202f4b',
+        },
+      },
+      fontFamily: {
+        sans: 'Inter, sans-serif',
+      },
     },
 
   },
