21685b8029033ab0b9710b1788beb374a8df316d
EmanuelRaffetto <emanuelraffetto18@gmail.com>
2024-10-29 15:18:25 -0300

    haciendo el header resposive

--- components/header.vue
+++ components/header.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+const menu = ref(false)
+
 const links = [
   {
     label: 'Descubrir',
@@ -18,11 +20,16 @@ const links = [
 <template>
   <div class="px-3 xl:px-0 fixed w-full top-0 z-1 print:hidden z-40">
     <header class="backdrop-blur-sm z-1 container lg:mx-auto max-w-6xl flex justify-between py-3 bg-gray-100 mt-8 rounded-full pl-8 pr-3 items-center">
-      <h2 class="text-black text-3xl font-bold underline decoration-[#ecdd7b]">
-        Linkin*
-      </h2>
-      
-       <nav class="md:block hidden">
+      <div>
+        <h2 class="text-black sm:block hidden text-3xl font-bold underline decoration-[#ecdd7b]">
+          Linkin*
+        </h2>
+        <h2 class="text-black sm:hidden block text-3xl font-bold underline decoration-[#ecdd7b]">
+          in*
+        </h2>
+      </div>
+
+      <nav class="md:block hidden">
         <ul class="gap-x-4 flex">
           <li v-for="link in links" :key="link.href">
             <NuxtLink :to="link.href" class="text-[16px] buttons px-2 font-medium text-customgray">
@@ -39,6 +46,9 @@ const links = [
         >
           Crea tu cuenta
         </a>
+        <button class="text-4xl sm:hidden order-3 flex items-center pr-3" @click="menu = !menu">
+          <Icon :name=" menu ? 'i-lucide-x' : 'i-ph-list'" />
+        </button>
       </div>
     </header>
   </div>
