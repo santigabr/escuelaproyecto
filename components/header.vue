f1117924cbdb7738225ffc04e6aa2714690c6581
Santino <89117328+santigabr@users.noreply.github.com>
2024-10-29 14:28:01 -0700

    Estructura header

---
+++ components/header.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="">
+    <header class="x">
+      <h2 class="">
+        Linkin*
+      </h2>
+
+      <div class="">
+        <a
+          class=""
+          href=""
+        >
+          Crea tu cuenta
+        </a>
+      </div>
+    </header>
+  </div>
+</template>
