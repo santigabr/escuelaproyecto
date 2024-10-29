dfc0477ec9e5e86af2163824470c152ecd627d69
EmanuelRaffetto <emanuelraffetto18@gmail.com>
2024-10-29 14:48:07 -0300

    Agregando animaciones

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <main class="bg-rhino-200">
-    <div class="w-full min-h-screen lg:grid lg:grid-cols-2 place-content-center lg:mx-auto gap-5 justify-center max-w-6xl px-3 xl:px-0">
+    <div class="w-full min-h-screen animate-slidein opacity-0 [--slidein-delay:200ms] lg:grid lg:grid-cols-2 place-content-center lg:mx-auto gap-5 justify-center max-w-6xl px-3 xl:px-0">
       <div class="flex flex-col justify-center gap-5">
         <h2 class="text-6xl pb-2 font-[900] text-rhino-950 max-w-[500px] tracking-tighter">
           Todo lo que eres. En un simple enlace.
