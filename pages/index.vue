2ebe63167686364c9b719e86a5c4fd0de47dfe48
Santino <89117328+santigabr@users.noreply.github.com>
2024-10-29 14:26:53 -0700

    Estructura pagina principal

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <main>
+    <div>
+      <div>
+        <h2>
+          Todo lo que eres. En un simple enlace.
+        </h2>
+        <p>
+          Un enlace para ayudarte a compartir todo lo que creas, creas y vendes desde tus perfiles de Instagram, TikTok, Twitter, YouTube y otras redes sociales.
+        </p>
+        <div>
+          <Button href="/hola">
+            Reclama tu linkin!
+          </Button>
+        </div>
+      </div>
+      <div>
+        <NuxtImg class="lg:block hidden" src="/linkbio.webp" />
+      </div>
+    </div>
+  </main>
+</template>
