ee2e06481c23ddc4ba49dbcdc0a9ba2c6125ad9a
Santino <89117328+santigabr@users.noreply.github.com>
2024-10-29 14:25:37 -0700

    Estilos componente boton

--- components/button.vue
+++ components/button.vue
@@ -3,7 +3,7 @@
     title="Comprar libro"
     target="_blank"
     rel="noopener noreferrer"
-    class=""
+    class="text-lg font-bold bg-rhino-700 hover:saturate-150 transition text-white rounded-full px-[26px] py-[18px] place-self-start"
   >
     <slot />
   </a>
