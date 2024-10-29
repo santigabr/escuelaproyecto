a5771920817a04cfec9d54240297c5654ad553f5
Santino <89117328+santigabr@users.noreply.github.com>
2024-10-29 14:28:17 -0700

    Estilos header

--- components/header.vue
+++ components/header.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="">
-    <header class="x">
-      <h2 class="">
+  <div class="px-3 xl:px-0 fixed w-full top-0 z-1 print:hidden">
+    <header class="backdrop-blur-sm z-1 container lg:mx-auto max-w-6xl flex justify-between py-3 bg-gray-100 mt-8 rounded-full pl-8 pr-3 items-center">
+      <h2 class="text-black text-3xl font-bold underline decoration-[#ecdd7b]">
         Linkin*
       </h2>
 
-      <div class="">
+      <div class="flex gap-2">
         <a
-          class=""
+          class="text-[16px] font-bold bg-rhino-700 hover:saturate-150 transition text-white rounded-full px-[26px] py-[18px]"
           href=""
         >
           Crea tu cuenta
